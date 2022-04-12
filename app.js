51170cc13748e1d8d11b046d738b590c91c5a313
Mohamed <mohammed.eldeib@gmail.com>
2022-04-13 00:54:04 +0200

    fixed Bug

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
-    console.log(query;
+    console.log(query);
 });
 
 server.listen(3000);
