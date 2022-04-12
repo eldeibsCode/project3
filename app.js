bb484f1a0effd785dcb1c585b5d691bdca47f91f
Mohamed <mohammed.eldeib@gmail.com>
2022-04-13 00:17:36 +0200

    save users data

--- app.js
+++ app.js
@@ -12,12 +12,23 @@ const server = http.createServer((req, res)=>{
         res.setHeader('Content-type', 'text/html');
         res.write('<html>');
         res.write('<head><title>my first response </title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
         res.write('</html>');
         return res.end();
     } 
     if (url ==='/message' && method=='POST'){
-        fs.writeFileSync('./usersData/message.txt', 'Dummy');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            // console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('./usersData/message.txt', message);
+        })  
+        
         res.statusCode=302;
         res.setHeader('Location', '/');
         return res.end();
