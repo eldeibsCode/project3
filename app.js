0e36dd1fc75ec5d7e5d0975e485ed993f2b8651f
Mohamed <mohammed.eldeib@gmail.com>
2022-04-12 23:21:57 +0200

    first response

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const querystring = require('querystring');
 
 const server = http.createServer((req, res)=>{
     console.log(req);
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>my first response </title></head>');
+    res.write('<body><h1>my first response </h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
