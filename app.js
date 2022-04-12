a0c8a40b8e8b7373eed5ca21cc1fbab6aec9c7e6
Mohamed <mohammed.eldeib@gmail.com>
2022-04-12 23:12:32 +0200

    parse url

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log(req.url)
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    console.log(query;
 });
 
 server.listen(3000);
