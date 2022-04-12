1323c4769713ee63329b209a542746fdb17d92ee
Mohamed <mohammed.eldeib@gmail.com>
2022-04-12 23:42:01 +0200

    handel form response

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const querystring = require('querystring');
 
 const server = http.createServer((req, res)=>{
     const url = req.url;
-    if (url === '/')
-    console.log(req);
+    if (url === '/'){
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>my first response </title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    } 
 });
 
 server.listen(3000);
