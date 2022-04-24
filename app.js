0d66836a9c3252efa57f9bbb2a0f072d8359d574
Mohamed <mohammed.eldeib@gmail.com>
2022-04-24 15:08:07 +0200

    add default Page for undefinded route

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) =>{
+    console.log("undefined router is visited!");
+    res.status(404).send("<h1> Page not found</h1>");
+});
 
 
 // const server = http.createServer(app);
