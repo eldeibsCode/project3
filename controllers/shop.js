445c66d5347242ef392a1582c61c03a90a93e04e
Mohamed <mohammed.eldeib@gmail.com>
2022-06-04 12:59:52 +0200

    addiing orders views & controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -28,6 +28,11 @@ exports.getCart = (req, res, next)=>{
         path: "/cart" });
 } 
 
+exports.getOrders = (req, res, next)=>{
+    res.render("shop/orders", { 
+        pageTitle: "Your Orders", 
+        path: "/orders" });
+} 
 exports.getCheckout = (req, res, next) => {
     res.render("shop/cart", {
         pageTitle: 'Checkout',
