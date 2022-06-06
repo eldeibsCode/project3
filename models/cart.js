5f60f04b3a42991081543a7bf88e236ad7073fa8
Mohamed <mohammed.eldeib@gmail.com>
2022-06-06 20:59:59 +0200

    enhance prices numbers

--- models/cart.js
+++ models/cart.js
@@ -36,7 +36,7 @@ module.exports = class Cart{
                 cart.products = [...cart.products, updatedProduct];
             }
             cart.totalPrice = +cart.totalPrice + +productPrice;
-
+            cart.totalPrice = cart.totalPrice.toFixed(2);
             fs.writeFile(p, JSON.stringify(cart), (err) => {
                 console.log(err);
             });
@@ -52,7 +52,7 @@ module.exports = class Cart{
             const productQty = product.qty;
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
             updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
-
+            updatedCart.totalPrice = updatedCart.totalPrice.toFixed(2);
             fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
                 console.log(err);
             });
