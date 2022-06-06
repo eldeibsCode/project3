d4ee09c3c7f282b3751df12c4d250010bf8cfb17
Mohamed <mohammed.eldeib@gmail.com>
2022-06-06 15:32:38 +0200

    adding query params

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,7 @@
 const Product = require('../models/products');
 
 exports.getAddProduct = (req, res, next) =>{
-    // console.log("in another middleware!");
-    // console.log(rootDir);
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-
-    res.render("admin/add-product", {
+    res.render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
     });
@@ -23,6 +19,19 @@ exports.postAddProduct =  (req, res, next) =>{
     product.save();
     res.redirect("/");
 }
+
+exports.getEditProduct = (req, res, next) =>{
+    // console.log(req.query);
+    const editMode = req.query.edit;
+    if(!editMode){
+        return res.redirect('/');
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode
+    });
+};
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll( products => {
