5465e99192b69fb4a72c57e0b6d1d3e58f5b182a
Mohamed <mohammed.eldeib@gmail.com>
2022-06-09 11:01:17 +0200

    Add fetching a single product with findAll

--- controllers/shop.js
+++ controllers/shop.js
@@ -22,11 +22,12 @@ exports.getProducts =  (req, res, next) =>{
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
     // console.log(prodId);
-    Product.findByPk(prodId)
+    // Product.findByPk(prodId)
+    Product.findAll({where: {id: prodId}})
         .then(product => {
-                // console.log(rows[0]);
+                // console.log(product[0]);
                 res.render('shop/product-detail',{
-                    product: product,
+                    product: product[0],
                     pageTitle: product.title,
                     path:'/products'
                 });
