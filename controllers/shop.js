e27d834d530c4cdff9abe2e088aa32aafdeea802
Mohamed <mohammed.eldeib@gmail.com>
2022-06-10 13:31:45 +0200

    Resetting the cart after order

--- controllers/shop.js
+++ controllers/shop.js
@@ -141,9 +141,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -155,6 +157,9 @@ exports.postOrder = (req, res, next) => {
             }));
         })
         .catch(err => console.log(err));
+    })
+    .then(result => {
+        fetchedCart.setProducts(null);
     })
     .then(result => {
         res.redirect('/order');
