71c6d903a793799e05daefcac696b80dc675e9a9
Mohamed <mohammed.eldeib@gmail.com>
2022-06-11 21:53:18 +0200

    Setting up user to requests

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressHbs  = require('express-handlebars');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
 
+const User = require("./models/user");
 
 const app = express();
 
@@ -25,40 +26,38 @@ const app = express();
 // app.set('view engine', 'hbs');
 
 // engine template: ejs
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 // const res = require('express/lib/response');
 
-
-app.use(bodyParser.urlencoded({extended: false}));
-// print all requests 
+app.use(bodyParser.urlencoded({ extended: false }));
+// print all requests
 // app.use('/', (req, res, next)=>{
 //     console.log('app:path:',req.url);
 //     next();
 // });
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then( user => {
-    //         req.user = user;
-    //         next();
-    //     })
-    //     .catch(err => console.log(err));
-    next();
+  User.findById("62a4eb7308cadb1e5715058f")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
-
-app.use('/admin', adminData);
+app.use("/admin", adminData);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
-});
+const user = new User(1, "Moha");
 
+mongoConnect(() => {
+  app.listen(3000);
+});
