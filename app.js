542178bd213cf9d5f679ddf428392dafdcf1ee8a
Mohamed <mohammed.eldeib@gmail.com>
2022-06-11 23:52:08 +0200

    Adding products to cart

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("62a4eb7308cadb1e5715058f")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
--- controllers/shop.js
+++ controllers/shop.js
@@ -72,37 +72,47 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-
-  let fetchedCart;
-  let newQuantity = 1;
-  req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then((products) => {
-      let product;
-      if (products.length > 0) {
-        product = products[0];
-      }
-
-      if (product) {
-        const oldQuantity = product.cartItem.quantity;
-        newQuantity = oldQuantity + 1;
-        return product;
-      }
-      return Product.findByPk(prodId);
-    })
+  // console.log('PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP', prodId);
+  Product.findById(prodId)
     .then((product) => {
-      return fetchedCart.addProduct(product, {
-        through: { quantity: newQuantity },
-      });
+      return req.user.addToCart(product);
     })
-    .then(() => {
-      res.redirect("/cart");
+    .then(result => {
+      console.log(result);
     })
-    .catch((err) => console.log(err));
+    // .catch((err) => console.log(err));
+
+
+  // let fetchedCart;
+  // let newQuantity = 1;
+  // req.user
+  //   .getCart()
+  //   .then((cart) => {
+  //     fetchedCart = cart;
+  //     return cart.getProducts({ where: { id: prodId } });
+  //   })
+  //   .then((products) => {
+  //     let product;
+  //     if (products.length > 0) {
+  //       product = products[0];
+  //     }
+
+  //     if (product) {
+  //       const oldQuantity = product.cartItem.quantity;
+  //       newQuantity = oldQuantity + 1;
+  //       return product;
+  //     }
+  //     return Product.findByPk(prodId);
+  //   })
+  //   .then((product) => {
+  //     return fetchedCart.addProduct(product, {
+  //       through: { quantity: newQuantity },
+  //     });
+  //   })
+  //   .then(() => {
+  //     res.redirect("/cart");
+  //   })
+  //   .catch((err) => console.log(err));
 
   // my approach
   // let cartId;
@@ -149,33 +159,36 @@ exports.postOrder = (req, res, next) => {
       return cart.getProducts();
     })
     .then((products) => {
-      return req.user.createOrder()
-        .then(order => {
-            order.addProduct(products.map(product => {
-                product.orderItem = {quantity: product.cartItem.quantity};
-                return product;
-            }));
+      return req.user
+        .createOrder()
+        .then((order) => {
+          order.addProduct(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
         })
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
     })
-    .then(result => {
-        fetchedCart.setProducts(null);
+    .then((result) => {
+      fetchedCart.setProducts(null);
     })
-    .then(result => {
-        res.redirect('/order');
+    .then((result) => {
+      res.redirect("/order");
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders({include: ['products']})
-    .then(orders => {
-        res.render("shop/orders", {
-            pageTitle: "Your Orders",
-            path: "/orders",
-            orders: orders
-          });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        pageTitle: "Your Orders",
+        path: "/orders",
+        orders: orders,
+      });
     })
-    .catch(err => console.log(err));
-  
+    .catch((err) => console.log(err));
 };
